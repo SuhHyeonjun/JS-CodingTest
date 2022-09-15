16fd1979c2584529ba48abfa273ec3c1a6f86ee7
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-09-15 21:00:49 +0900

    직사각형 나머지 한 점 구하기

--- Algorithms/algorithms3.js
+++ Algorithms/algorithms3.js
@@ -27,18 +27,31 @@ console.log(solution([[1, 1], [2, 2], [1, 2]]));
 /* 논리연산자 XOR(^)을 사용
 세 값 중 두 개가 같고 나머지 하나가 다를 경우 그 다른 값을 반환 */
 function solution(v) {
-    let answer = [
-        []
-    ];
+    let answer = [[]];   // 2차원 배열 
+
     
 // x좌표 구하기
-answer[0] = v[0][0] ^ v[1][0] ^ v[2][0];
+answer[0] = v[0][0] ^ v[1][0] ^ v[2][0]; // 두개 값이 같고 나머지 하나 값이 다를 시 다른 값 반환 (1, 3, 3)
     
 // y좌표 구하기
-answer[1] = v[0][1] ^ v[1][1] ^ v[2][1];
+answer[1] = v[0][1] ^ v[1][1] ^ v[2][1]; // 두개 값이 같고 나머지 하나 값이 다를 시 다른 값 반환 (4, 4, 10)
 
 return answer;
 }
 
 console.log(solution([[1, 4], [3, 4], [3, 10]]));
 console.log(solution([[1, 1], [2, 2], [1, 2]]));
+
+
+/* 1차원 배열의 경우
+
+    //push를 사용하는 경우
+    
+    var answer = []; // 1차원 배열
+    
+    answer.push(v[0][0]^v[1][0]^v[2][0]); // 배열의 요소로 넣음 
+    answer.push(v[0][1]^v[1][1]^v[2][1]);
+    
+    return answer;
+
+*/
