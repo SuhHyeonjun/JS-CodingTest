834ff89348145e8897cd3527621499a3058e8143
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-09-16 17:07:09 +0900

    가장 큰 정사각형 넓이 구하기

--- Algorithms/algorithms4.js
+++ Algorithms/algorithms4.js
@@ -5,29 +5,28 @@ function solution(board) {
     const row = board.length;
     const column = board[0].length;
   
-    if (row <= 1 || column <= 1) return 1;
+    if (row <= 1 || column <= 1) return 1; 
   
-    for (let i = 1; i < row; i++) {
-      for (let j = 1; j < column; j++) {
-        if (board[i][j] > 0) {
-          const up = board[i - 1][j];
+    for (let i = 1; i < row; i++) { // 1행부터 시작
+      for (let j = 1; j < column; j++) { // 1열부터 시작
+        if (board[i][j] > 0) { 
+          const up = board[i - 1][j]; 
           const left = board[i][j - 1];
           const cross = board[i - 1][j - 1];
-          const minNum = Math.min(up, left, cross);
+          const minNum = Math.min(up, left, cross); // 3개의 값 중 가장 작은 값
 
-          console.log(minNum);
-          console.log(minNum+1);
+          board[i][j] = minNum + 1; 
 
-          board[i][j] = minNum + 1;
-          answer = Math.max(answer, board[i][j]);
+          console.log(board[i][j]);
+
+          answer = Math.max(answer, board[i][j]); 
         }
       }
     }
   
-    return answer **2;
+    return answer **2; 
   }
 
 console.log("----------------------------");
     console.log(solution([[0, 1, 1, 1], [1, 1, 1, 1], [1, 1, 1, 1], [0, 0, 1, 0]]));
-    console.log(solution([[0, 0, 1, 1], [1, 1, 1, 1]]));
 console.log("----------------------------");
