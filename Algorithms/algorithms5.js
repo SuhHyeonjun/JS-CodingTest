5c274070f171c6189b9c5daede2274a0fc38d2ef
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-09-18 21:31:36 +0900

    가장 많은 문제를 맞춘 수포자 찾기

--- Algorithms/algorithms5.js
+++ Algorithms/algorithms5.js
@@ -23,24 +23,65 @@ answers	return
 모든 사람이 2문제씩을 맞췄습니다.
 */
 
+/* 방법 1 */
 function solution(answers) {
-  const one = [1, 2, 3, 4, 5];
-  const two = [2, 1, 2, 3, 2, 4, 2, 5];
-  const three = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
-  const result = [0, 0, 0];
-  for (let i = 0; i < answers.length; i++) {
-    if (one[i % 5] === answers[i]) result[0]++;
-    if (two[i % 8] === answers[i]) result[1]++;
-    if (three[i % 10] === answers[i]) result[2]++;
-  }
-  const answer = [];
-  const maxValue = Math.max(...result);
-  let index = 0;
-  for (let i = 0; i < 3; i++) {
-    if (maxValue === result[i]) {
-      answer[index] = i + 1;
-      index++;
+    let answer = [];
+    let one = [1, 2, 3, 4, 5];
+    let two = [2, 1, 2, 3, 2, 4, 2, 5]
+    let three = [ 3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
+    let result = [0, 0, 0];
+
+
+    for (let i = 0; i < answers.length; i++) {
+      if (one[i % 5] === answers[i]) result[0]++;
+      if (two[i % 8] === answers[i]) result[1]++;
+      if (three[i % 10] === answers[i]) result[2]++;
     }
-  }
-  return result;
+
+    let max = Math.max(result[0], result[1], result[2]);
+
+    if (result[0] === max) answer.push(1);
+    if (result[1] === max) answer.push(2);
+    if (result[2] === max) answer.push(3);
+
+
+    return answer;
 }
+
+console.log(solution([1, 2, 3, 4, 5]));
+console.log(solution([1, 3, 2, 4, 2]));
+
+/* 방법 2 */
+function solution(answers) {
+  let answer = [];
+
+    // 수포자들의 답안 패턴
+  let one = [1, 2, 3, 4, 5];
+  let two = [2, 1, 2, 3, 2, 4, 2, 5]
+  let three = [ 3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
+
+    // 맞춘 정답의 개수를 저장
+  let oneResult = 0;
+  let twoResult = 0;
+  let threeResult = 0;
+
+    // 정답과 수포자들의 답안을 비교
+  for (let i = 0; i < answers.length; i++) {
+    if (one[i % 5] === answers[i]) oneResult++;
+    if (two[i % 8] === answers[i]) twoResult++;
+    if (three[i % 10] === answers[i]) threeResult++;
+  }
+
+    // 가장 많이 맞춘 정답의 개수
+  let max = Math.max(oneResult, twoResult, threeResult);
+
+  if (oneResult === max) answer.push(1);
+  if (twoResult === max) answer.push(2);
+  if (threeResult === max) answer.push(3);
+
+
+  return answer;
+}
+
+console.log(solution([1, 2, 3, 4, 5]));
+console.log(solution([1, 3, 2, 4, 2]));
