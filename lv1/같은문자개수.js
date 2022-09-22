6aad9268bebcfa364a3c6bdefcd8663b53045844
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-09-22 23:33:05 +0900

    Lv1 같은문자개수: match 함수 추가

--- lv1/같은문자개수.js
+++ lv1/같은문자개수.js
@@ -11,3 +11,24 @@ function solution(s){
 
 console.log(solution("pPoooyY"));
 console.log(solution("Pyy"));
+
+/* .match() 사용 */
+function solution(s){
+    let answer = true;
+
+    // .match(/찾을값/옵션)
+    // 옵션 : /g 전체 찾기, /i 대소문자 구분 안함, /m 여러줄 찾기, /u 유니코드 찾기, gi 전체 찾기 대소문자 구분 안함)
+    let checkp = s.match(/p/gi).length;
+    let checky = s.match(/y/gi).length;
+
+    console.log(checkp)
+    console.log(checky)
+
+    
+    if (checkp === 0 && checky === 0) return true;
+    return answer = checkp === checky ? true : false;
+    
+}
+
+console.log(solution("pPoooyY"));
+console.log(solution("Pyy"));
