951a2a115afd3d39a82095f625cec6a63b4b45e9
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-09-24 13:49:37 +0900

    x만큼 간격이 있는 n개의 숫자

---
+++ lv1/x만큼간격n.js
@@ -0,0 +1,12 @@
+function solution(x, n) {
+    let answer = [];
+    for (let i = 0; i < n; i++) {
+        answer.push(x * (i + 1));
+    }
+    return answer;
+}
+
+
+console.log(solution(2, 5));
+console.log(solution(4, 3));
+console.log(solution(-4, 2));
