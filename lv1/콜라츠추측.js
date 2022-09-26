33a7f78b9a2c5ecabcf226077c82340ed0e03df4
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-09-26 20:43:40 +0900

    콜라츠추측

---
+++ lv1/콜라츠추측.js
@@ -0,0 +1,18 @@
+function solution(num) {
+    let answer = 0
+    while(num != 1) {
+        if (num%2==0) {
+             num = num/2;
+        } else {
+             num = num * 3 + 1;
+        } 
+         answer++;
+        
+        if (answer > 500) return -1
+    }
+      return answer;
+  }
+
+console.log(solution(6));
+console.log(solution(16));
+console.log(solution(626331));
