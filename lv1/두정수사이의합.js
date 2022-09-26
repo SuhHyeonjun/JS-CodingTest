7b0e7ee8d857fa4c1973c5c762fa37c4c317ea19
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-09-26 20:48:48 +0900

    lv1 두정수사이의 합 구하기

---
+++ lv1/두정수사이의합.js
@@ -0,0 +1,17 @@
+function solution(a, b){
+    let answer = 0;
+    if (a > b) {
+        for (let i = b; i <= a; i++) {
+            answer += i;
+        }
+    } else {
+        for (let i = a; i <= b; i++) {
+            answer += i;
+        }
+    } 
+    return answer;
+}
+
+console.log(solution(3, 5));
+console.log(solution(3, 3));
+console.log(solution(5, 3));
