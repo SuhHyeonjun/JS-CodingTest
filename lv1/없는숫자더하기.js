bf8d15fa7ff0d638b24642b2c03f090cbf0bb0dc
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-09-27 20:03:05 +0900

    없는숫자 더하기 : includes 함수 사용

--- lv1/없는숫자더하기.js
+++ lv1/없는숫자더하기.js
@@ -1,11 +1,24 @@
-function solution(numbers) {
-    var answer = -1;
-    var sum = 0;
-    for(var i=0; i<numbers.length; i++){
-        sum += numbers[i];
-    }
-    answer = 45 - sum;
+// function solution(numbers) {
+//     var answer = -1;
+//     var sum = 0;
+//     for(var i=0; i<numbers.length; i++){
+//         sum += numbers[i];
+//     }
+//     answer = 45 - sum;
 
+//     return answer;
+// }
+
+
+// console.log(solution([1,2,3,4,6,7,8,0]));
+// console.log(solution([5,8,4,0,6,7,9]));
+
+/* 다른 풀이 */
+function solution(numbers) {
+    let answer = 0;
+    for (let i = 0; i < 10; i++) {
+        if (!numbers.includes(i)) answer += i;
+    }
     return answer;
 }
 
