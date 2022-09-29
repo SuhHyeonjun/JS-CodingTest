9551f0915199e5d21563fde91a7aeacb40d00ad0
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-09-29 20:19:49 +0900

    3진법, 10진법 계산

--- lv1/3진법뒤집기.js
+++ lv1/3진법뒤집기.js
@@ -5,6 +5,23 @@ function solution(n) {
 
 }
 
-
-
 console.log(solution(45));
+
+/* 진법 변환
+
+let value = 10;
+
+value.toString(2);    // 1010
+value.toString(8);    // 12
+value.toString(16);   // a
+
+var bin = 1010,
+    oct = 12,
+    hex = 'a';
+
+// 2, 8, 16 진법 -> 10진법으로 변환
+Number.parseInt(bin, 2);    // 10
+Number.parseInt(oct, 8);    // 10
+Number.parseInt(hex, 16);   // 10
+
+*/
