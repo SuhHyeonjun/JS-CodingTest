65035ac6b2ba864835d08b25f593693ca01c38ee
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-09-30 21:35:34 +0900

    최소직사각형 값 구하기: 풀이 이유 추가

--- lv1/최소직사각형.js
+++ lv1/최소직사각형.js
@@ -2,6 +2,8 @@ function solution(sizes) {
     let w = []; // 가로
     let h = []; // 세로
 
+
+    // 길이가 큰 것이 가로, 작은 것이 세로가 될 것이다.
     for (let i = 0; i < sizes.length; i++) {
         let max = Math.max(sizes[i][0], sizes[i][1]);  // 가로, 세로 중 큰 값
         let min = Math.min(sizes[i][0], sizes[i][1]); // 가로, 세로 중 작은 값
